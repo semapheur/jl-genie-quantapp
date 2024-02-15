650de01d5ec2b8363c6cae356618e03378923913
semapheur <danfyll@hotmail.com>
2024-02-15 18:53:27 +0100

    Deploy

--- app.jl
+++ app.jl
@@ -5,7 +5,7 @@ using GenieFramework
 include("pages/Index.jl")
 include("api/StockTicker.jl")
 
-#Stipple.Layout.add_script("https://cdn.tailwindcss.com")
+Stipple.Layout.add_script("https://cdn.tailwindcss.com")
 
 @page("/", Index.ui, model = Index)
 route("/api/search/ticker/stock/:query::String", StockTicker.search, method=GET)
--- pages/Index.jl
+++ pages/Index.jl
@@ -6,23 +6,58 @@ using Main.Morningstar
 
 @genietools
 
+function date_input(date_var::Symbol, label::String, mask="YYYY-MM-DD")
+  textfield(
+    label,
+    date_var,
+    clearable=true,
+    filled=true,
+    [
+      icon(
+        "event",
+        class="cursor-pointer",
+        [popupproxy([datepicker(date_var, label=label, todaybtn=true, mask=mask)])],
+      ),
+    ],
+  )
+end
+
+_start_date = "2000-01-01"
+_end_date = Dates.format(Dates.today(), "yyyy-mm-dd")
+
 @app begin
   @out stocks = Vector{Dict{String,String}}()
   @in selected_stock = ""
-  @in start_date = "2000-01-01"
-  @in end_date = Dates.format(Dates.today(), "yyyy-mm-dd")
+  @in start_date = _start_date
+  @in end_date = _end_date
   @out ohlcv = DataFrame(date=[], open=[], high=[], low=[], close=[], volume=[])
-  @out plot_data =
-    Base.invokelatest(PlotlyBase.GenericTrace{Dict{Symbol,Any}}, Dict{Symbol,Any}())
-  @out plot_layout = PlotlyBase.Layout()
+  @out plot_data = Vector{PlotlyBase.GenericTrace{Dict{Symbol,Any}}}()
+  @out plot_layout = PlotlyBase.Layout(
+    xaxis=attr(
+      title="Date",
+      rangeselector=attr(
+        buttons=[
+          attr(count=1, label="1m", step="month", stepmode="backward"),
+          attr(count=6, label="6m", step="month", stepmode="backward"),
+          attr(count=1, label="YTD", step="year", stepmode="todate"),
+          attr(count=1, label="1y", step="year", stepmode="backward"),
+          attr(step="all"),
+        ],
+      ),
+    ),
+    yaxis=attr(title="Close Price", range=[_start_date, _end_date]),
+  )
+
+  #Base.invokelatest(PlotlyBase.Layout{Dict{Symbol,Any}}, Dict{Symbol,Any}())
 
   @onchange selected_stock begin
     id, currency = split(selected_stock, "_")
     ohlcv = Morningstar.ohlcv(string(id), string(currency))
-    plot_data = scatter(x=ohlcv.date, y=ohlcv.close, mode="lines")
-    plot_layout = PlotlyBase.Layout(
-      xaxis=attr(title="Date"),
-      yaxis=attr(title="Close Price [$currency]"),
+    plot_data = [scatter(x=ohlcv.date, y=ohlcv.close, mode="lines")]
+    relayout!(
+      plot_layout,
+      xaxis_range=[ohlcv.date[1], ohlcv.date[end]],
+      yaxis=attr(title="Close Price [$currency]", range=[ohlcv.close[1], ohlcv.close[end]]),
     )
   end
 
@@ -51,51 +86,31 @@ filterFn: async function(val, update, abort) {
 """
 
 function ui()
-  htmldiv([
-    Genie.Renderer.Html.select(
-      :selected_stock,
-      options=:stocks,
-      optionlabel="label",
-      optionvalue="value",
-      emitvalue=true,
-      mapoptions=true,
-      useinput=true,
-      inputdebounce=500,
-      clearable=true,
-      label="Ticker",
-      var"@filter"="filterFn",
-    ),
-    textfield(
-      "Start date",
-      :start_date,
-      clearable=true,
-      filled=true,
-      [
-        icon(
-          "event",
-          class="cursor-pointer",
-          [popupproxy([datepicker(:start_date, label="Start Date", mask="YYYY-MM-DD")])],
-        ),
-      ],
-    ),
-    textfield(
-      "End date",
-      :end_date,
-      clearable=true,
-      filled=true,
-      [
-        icon(
-          "event",
-          class="cursor-pointer",
-          [
-            popupproxy([
-              datepicker(:end_date, label="End Date", todaybtn=true, mask="YYYY-MM-DD"),
-            ]),
-          ],
-        ),
-      ],
-    ),
-    plot(:plot_data, layout=:plot_layout),
-  ],)
+  htmldiv(
+    class="h-full grid grid-cols-[1fr_3fr]",
+    [
+      htmldiv(
+        class="h-full flex flex-col gap-2",
+        [
+          Genie.Renderer.Html.select(
+            :selected_stock,
+            options=:stocks,
+            optionlabel="label",
+            optionvalue="value",
+            emitvalue=true,
+            mapoptions=true,
+            useinput=true,
+            inputdebounce=500,
+            clearable=true,
+            label="Ticker",
+            var"@filter"="filterFn",
+          ),
+          date_input(:start_date, "Start Date"),
+          date_input(:end_date, "End Date"),
+        ],
+      ),
+      plot(:plot_data, layout=:plot_layout, class="h-full", style="position:static"),
+    ],
+  )
 end
 end
